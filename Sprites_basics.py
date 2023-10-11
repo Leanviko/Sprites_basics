9b4c90a0d8b2917be7c34d982b299c9af4255454
LeanViko <leangant@gmail.com>
2023-10-10 22:17:22 -0300

    estructura basica de pygame

--- Sprites_basics.py
+++ Sprites_basics.py
@@ -1,2 +1,22 @@
 import pygame
 import sys 
+
+# General Setup
+
+pygame.init()
+clock = pygame.time.Clock()
+
+#Game Screen
+screen_width = 1920
+screen_height = 1080
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.flip()
+    clock.tick(60)
