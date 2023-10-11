6fb0f0cea3c024d9443050d2bab58e76701dc110
LeanViko <leangant@gmail.com>
2023-10-10 22:38:44 -0300

    primera clase y grupo

--- Sprites_basics.py
+++ Sprites_basics.py
@@ -1,8 +1,16 @@
 import pygame
 import sys 
 
-# General Setup
+#Classes
+class Crosshair(pygame.sprite.Sprite):
+    def __init__(self, width, height,pos_x, pos_y, color):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.center = [pos_x, pos_y]
 
+# General Setup
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -10,6 +18,12 @@ clock = pygame.time.Clock()
 screen_width = 1920
 screen_height = 1080
 screen = pygame.display.set_mode((screen_width,screen_height))
+
+crosshair = Crosshair(50,50,100,100,(255,255,255))
+
+#Create a group 
+crosshair_group = pygame.sprite.Group()
+crosshair_group.add(crosshair)
 
 
 while True:
@@ -19,4 +33,5 @@ while True:
             sys.exit()
 
     pygame.display.flip()
+    crosshair_group.draw(screen)
     clock.tick(60)
